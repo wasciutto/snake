add93e0a6356ae7076f830434b3cf4fd4baf9326
William Asciutto <wjasciutto@gmail.com>
2019-08-07 21:31:40 -0400

    Replace numpy with simple 2d array.

--- SnakeGame.py
+++ SnakeGame.py
@@ -14,7 +14,7 @@ class SnakeGame:
         self.turn = 0
         self.snakeLength = 0
         self.validGame = True
-        self.gameBoard = np.zeros((SnakeGame.boardSize, SnakeGame.boardSize))
+        self.gameBoard = [[0 for x in range(SnakeGame.boardSize)] for y in range(SnakeGame.boardSize)]
 
     #Set up the snake in a random position
     def newGame(self):
@@ -22,41 +22,38 @@ class SnakeGame:
         #Range assures enough room for body without bounds checking
         startX = random.randint(4, SnakeGame.boardSize - 5)
         startY = random.randint(4, SnakeGame.boardSize - 5)
-        self.gameBoard.itemset((startX, startY), SnakeGame.snakeHead)
+        self.gameBoard[startX][startY] = SnakeGame.snakeHead
 
         #Choose a direction to build the body
         randDirection = random.randint(0, 3)
         #Build up
         if(randDirection == 0):
             for i in range(1, 5):
-                self.gameBoard.itemset((startX - i, startY), SnakeGame.snakeBody)
+                self.gameBoard[startX - i][startY] = SnakeGame.snakeBody
         #Build down
         elif(randDirection == 1):
             for i in range(1, 5):
-                self.gameBoard.itemset((startX + i, startY), SnakeGame.snakeBody)
+                self.gameBoard[startX + i][startY] = SnakeGame.snakeBody
         #Build left
         elif(randDirection == 2):
             for i in range(1, 5):
-                self.gameBoard.itemset((startX, startY - i), SnakeGame.snakeBody)
+                self.gameBoard[startX][startY - i] = SnakeGame.snakeBody
         #Build right
         elif(randDirection == 3):
             for i in range(1, 5):
-                self.gameBoard.itemset((startX, startY + i), SnakeGame.snakeBody)
+                self.gameBoard[startX - i][startY + i] = SnakeGame.snakeBody
 
     #Print the game board to console
     def printGame(self):
-        print(np.matrix(self.gameBoard))
+        for i in range(len(self.gameBoard)):
+            print(self.gameBoard[i])
 
     def checkSnakeSize(self):
         pass
 
     def checkSnakeEating(self):
         pass
-
-    #Returns the position of the snake head
-    def findHead(self):
-        pass
-
+    
     #Resets the game if invalid state is invalid
     def voidGame(self):
         pass
@@ -65,3 +62,4 @@ if __name__ == "__main__":
     game = SnakeGame()
     game.newGame()
     game.printGame()
+    print(game.findHead())
