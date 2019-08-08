3f80579c55ee580a8e9aae914272a7c1eb52bd34
William Asciutto <wjasciutto@gmail.com>
2019-08-07 22:30:13 -0400

    Implement 'GET' method.

--- RestApp.py
+++ RestApp.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from SnakeGame import SnakeGame
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class SnakeSimple(Resource):
+    def get(self):
+        game = SnakeGame()
+        game.newGame()
+        return {'turn':game.turn,'snakeLength':game.snakeLength,'validGame':game.validGame,
+    'gameBoard':json.dumps(game.gameBoard)}
+
+    def put(self):
+        return "POST"
+
+api.add_resource(SnakeSimple, "/")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
