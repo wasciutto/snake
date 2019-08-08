87acc66e47f6dfdc88f68e9ea5e7223850cee3d0
William Asciutto <wjasciutto@gmail.com>
2019-08-07 20:15:38 -0400

    Add comments to starting point methods.

--- SnakeGame.py
+++ SnakeGame.py
@@ -4,15 +4,18 @@ class SnakeGame:
 
     boardSize = 16
 
+    #Set up an empty game board
     def __init__(self):
         self.turn = 0
         self.snakeLength = 0
         self.validGame = True
         self.gameBoard = np.zeros((SnakeGame.boardSize, SnakeGame.boardSize))
 
+    #Set up the snake in a random position
     def newGame(self):
         pass
 
+    #Print the game board to console
     def printGame(self):
         print(np.matrix(self.gameBoard))
 
